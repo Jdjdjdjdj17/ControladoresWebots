18b545f03157dc971b11478b26dfdddd39924106
Jdjdjdjdj17 <jupasandoval17@gmail.com>
2026-09-09 00:17:31 -0600

    Pendulo

---
+++ controllers/pendulo/pendulo.cpp
@@ -0,0 +1,36 @@
+#include <webots/Robot.hpp>
+#include <webots/PositionSensor.hpp>
+#include <webots/Motor.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace webots;
+
+int main(int argc, char **argv) {
+  // create the Robot instance.
+  Robot *robot = new Robot();
+
+  // get the time step of the current world.
+  int timeStep = (int)robot->getBasicTimeStep();
+
+  Motor *motor = robot->getMotor("motor");
+  motor->setPosition(INFINITY);
+  motor->setForce(0.0);
+  
+  PositionSensor *ps = robot->getPositionSensor("encoder");
+  ps->enable(timeStep);
+
+  // Main loop:
+  // - perform simulation steps until Webots is stopping the controller
+  while (robot->step(timeStep) != -1) {
+    double val = ps->getValue();
+    cout << "Posicion Actual: " << val << " | Referencia: " << val << endl;
+
+    motor->setPosition(6.28);
+  };
+
+  // Enter here exit cleanup code.
+
+  delete robot;
+  return 0;
+}
