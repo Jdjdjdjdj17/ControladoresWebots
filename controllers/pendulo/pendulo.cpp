f36904b65651759d406ca99c84a33f5a4b88768f
Jdjdjdjdj17 <jupasandoval17@gmail.com>
2026-09-19 01:24:37 -0600

    Pendulo

--- controllers/pendulo/pendulo.cpp
+++ controllers/pendulo/pendulo.cpp
@@ -1,7 +1,8 @@
+
 #include <webots/Robot.hpp>
-#include <webots/PositionSensor.hpp>
-#include <webots/Motor.hpp>
-#include <iostream>
+#include<webots/PositionSensor.hpp>
+#include<webots/Motor.hpp>
+#include<iostream>
 
 using namespace std;
 using namespace webots;
@@ -13,20 +14,24 @@ int main(int argc, char **argv) {
   // get the time step of the current world.
   int timeStep = (int)robot->getBasicTimeStep();
 
-  Motor *motor = robot->getMotor("motor");
-  motor->setPosition(INFINITY);
-  motor->setForce(0.0);
-  
-  PositionSensor *ps = robot->getPositionSensor("encoder");
-  ps->enable(timeStep);
-
+  // You should insert a getDevice-like function in order to get the
+  // instance of a device of the robot. Something like:
+    Motor *motor = robot->getMotor("motor");
+     motor->setPosition(INFINITY);
+     motor->setForce(0);
+     
+      PositionSensor *ps = robot->getPositionSensor("encoder");
+       ps->enable(timeStep);
   // Main loop:
   // - perform simulation steps until Webots is stopping the controller
   while (robot->step(timeStep) != -1) {
+    // Read the sensors:
+    // Enter here functions to read sensor data, like:
     double val = ps->getValue();
-    cout << "Posicion Actual: " << val << " | Referencia: " << val << endl;
-
-    motor->setPosition(6.28);
+    cout<<"Posicion Actual: " << val<<endl;
+    // Process sensor data here.
+    // Enter here functions to send actuator commands, like:
+     
   };
 
   // Enter here exit cleanup code.
